a970a27bae397c7d71fc1cdcf9e4c992524f74d9
LuizCorrea-Dev <correa.luizbatista@gmail.com>
2021-06-19 17:09:24 -0300

    teste de api2

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,7 +2,7 @@ function tempo(request, response) {
   const dynamicDate = new Date()
 
   response.json({
-    date: dynamicDate.toGMTSString()
+    date: dynamicDate.toGMTString()
   })
 }
 // rota: site.com/api/tempo
