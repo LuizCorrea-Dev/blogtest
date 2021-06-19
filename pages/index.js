6d3d777a9e3f3b7aab4c66e092278c3bdfa6482d
LuizCorrea-Dev <correa.luizbatista@gmail.com>
2021-06-19 16:40:43 -0300

    teste de links

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
+import Link from 'next/link';
+
 function Home() {
-  return <div>Home</div>
+    return (<div>
+        <h1>Home</h1>
+
+        <Link href="/sobre">
+            <a>Acessar página Sobre</a>
+        </Link>
+    </div>)
 }
+
 export default Home
